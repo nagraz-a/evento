42393fcba439b64a9ef8976274e28533ae7d4281
lakshmiramy4 <lakshmiramya66@gmail.com>
2019-11-09 10:41:18 +0530

    upadate

--- Evento/src/main/java/com/datafoundry/evento/event/EventController.java
+++ Evento/src/main/java/com/datafoundry/evento/event/EventController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EventController {
 	
-	@RequestMapping("/hello")
+	@RequestMapping("/hello.....")
 	public String sayHi() {
 		return "Welcome to Evento";
 	}
