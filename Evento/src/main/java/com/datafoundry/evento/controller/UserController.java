93f561d1fa2cc459ffb6abfcf4cd50c42123d778
lakshmiramy4 <lakshmiramya66@gmail.com>
2019-11-11 15:08:20 +0530

    controller is updated for login

--- Evento/src/main/java/com/datafoundry/evento/controller/UserController.java
+++ Evento/src/main/java/com/datafoundry/evento/controller/UserController.java
@@ -3,11 +3,14 @@ package com.datafoundry.evento.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.datafoundry.evento.model.User;
@@ -26,9 +29,33 @@ public class UserController {
 		return "User Saved Sucessfully........";
 		
 	}
-	@GetMapping("getUser/{email}")
+	@GetMapping("/getUser/{email}")
 	public User getUserbyEmail(@PathVariable String email){
 		return repository.findByEmail(email);
 	}
-
+	@GetMapping("/getlogin/{email}/{password}")
+	   public String getUserBylogin(@PathVariable String email,@PathVariable String password) {
+		User user=repository.findByEmail(email);
+	   if(user==null)
+	   {
+		   
+		   return "Invalid Username or Password";
+		   
+		  
+	   }
+	   
+	   else if(user.getEmail().equals(email) && user.getPassword().equals(password) ) {
+			   return "login is successfull";
+		   }
+	   return "login";
+	}
 }
+//	   if(status==true)
+//	   {
+//		   modelMap.addAttribute("msg",msg);
+//		   return "findlogin";
+//	   }
+//	   modelMap.addAttribute("msg",msg);
+//	   return "login";
+//	   }
+
