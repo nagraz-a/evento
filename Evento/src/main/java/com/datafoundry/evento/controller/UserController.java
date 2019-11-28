ad34a1fef02874e364a95d17487a4570cd8369b2
lakshmiramy4 <lakshmiramya66@gmail.com>
2019-11-28 13:16:22 +0530

    Changes in project for frontend connection

--- Evento/src/main/java/com/datafoundry/evento/controller/UserController.java
+++ Evento/src/main/java/com/datafoundry/evento/controller/UserController.java
@@ -3,6 +3,7 @@ package com.datafoundry.evento.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import com.datafoundry.evento.service.UserService;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins="http://localhost:4200")
 public class UserController {
 	
 	@Autowired
