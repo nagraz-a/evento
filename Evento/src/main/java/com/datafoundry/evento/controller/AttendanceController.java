ef79c515642f92e689df6a50f962c2db526b985a
nagapravallika <nagapravallikaraja@gmail.com>
2019-12-12 15:00:44 +0530

    modification of attendance controller

--- Evento/src/main/java/com/datafoundry/evento/controller/AttendanceController.java
+++ Evento/src/main/java/com/datafoundry/evento/controller/AttendanceController.java
@@ -2,8 +2,14 @@ package com.datafoundry.evento.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +46,15 @@ public class AttendanceController
 		return eventRegistartionService.findAll();
 	}
 	
-
+	@PutMapping("/createAttendance/{id}")
+	public void updateAttendance(@PathVariable("id") String id, @Valid @RequestBody EventRegistration eventRegistration) {
+		eventRegistration.setId(id);
+		eventRegistartionService.save(eventRegistration);
+	//return " Attendace saved successfully";
+	}
+	
+	 	  
 }
+	
+
+
